199e05f3a2487fc278cce08b6c957715d285d60a
Alex <loujessler@milan-holidays.net>
2020-03-04 01:23:01 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import Header from './Components/Header/Header'
 import Nav from './Components/Nav/Nav'
 import Profile from './Components/Profile/Profile'
 import Footer from './Components/Footer/Footer'
+import Messages from "./Components/Messages/Messages";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Settings/Settings";
 
 const App = () => {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <Profile />
-      {/*<Footer /> */}
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Nav/>
+                <div className="gridContent">
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/messages' component={Messages}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+                {/*<Footer /> */}
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
